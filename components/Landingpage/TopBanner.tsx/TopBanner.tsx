7ef3417e10011fad3a4420be8de44925e9eccae2
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2025-01-15 19:05:41 +0600

    Refactor TopBanner component for improved text positioning and accessibility; enhance button styling and focus states

--- components/Landingpage/TopBanner.tsx/TopBanner.tsx
+++ components/Landingpage/TopBanner.tsx/TopBanner.tsx
@@ -6,11 +6,18 @@ export default function TopBanner() {
         <main className="container max-w-screen-2xl mx-auto">
             <div className="relative">
                 <Image src={topbannerData.image} alt={topbannerData.title} className="max-h-screen" />
-                <div className="absolute bottom-20 left-0 horizantalPadding text-white">
+                <div className="absolute bottom-7 md:bottom-20 left-0 horizantalPadding text-white">
                     <p className="text-2xl md:text-4xl xl:text-5xl text-white lg:w-2/3">{topbannerData.title}</p>
-                    <button className="mt-10 text-white border border-white rounded-full py-2 px-5 hover:bg-lightBlack hover:text-white hover:border-lightBlack transition-all">
-                        Book now
+                    <button
+                        aria-label="Book a service"
+                        className="mt-5 md:mt-10 text-white border border-white rounded-full py-2 px-5 
+              hover:bg-lightBlack hover:text-white hover:border-lightBlack 
+              focus:outline-none focus:ring-2 focus:ring-lightBlack 
+              focus:ring-offset-2 transition-all"
+                    >
+                        Book Now
                     </button>
+
                 </div>
             </div>
         </main>
